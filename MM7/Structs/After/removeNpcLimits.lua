eb11b61110754bf753fc63557e40bb41044ecd8f
Eksekk <eksekk3@gmail.com>
2023-07-12 21:33:57 +0200

    More refs

--- MM7/Structs/After/removeNpcLimits.lua
+++ MM7/Structs/After/removeNpcLimits.lua
@@ -41,37 +41,67 @@ do
 		NPCGroup:       low = 0x73BFAA    high = 0x73C010   size = 0x66       itemSize = 0x2
 		NPCProfNames:   low = 0x73C110    high = 0x73C1FC   size = 0xEC       itemSize = 0x4
 	]]
+
+	-- maybe unrelated address: 0x4764B4, references 0x724048
+
+	-- FIXME: NPCProfNames wasn't included in initial search!
 	
 	-- npc tables data range: 0x724004 - 0x73C027
 	local npcLimitRefs = { -- [cmd offset] = {addresses...}
-		[2] = {0x416AE4, 0x416B3D, 0x420C18, 0x420C6F, 0x0042E25B, 0x0042E2D0, 0x0043067B, 0x004306F0, 0x00445AC4, 0x00445B1F, 0x00445C06, 0x00445C67, 0x00445CFE, 0x00445D53, 0x00446132, 0x00446187, 0x0044A597, 0x0044A5E3, 0x0044BF0E, 0x0044BF2E},
+		[2] = {0x416AE4, 0x416B3D, 0x420C18, 0x420C6F, 0x0042E25B, 0x0042E2D0, 0x0043067B, 0x004306F0, 0x00445AC4, 0x00445B1F, 0x00445C06, 0x00445C67, 0x00445CFE, 0x00445D53, 0x00446132, 0x00446187, 0x0044A597, 0x0044A5E3, 0x0044BF0E, 0x0044BF2E, 0x00463520, 0x00463539, 0x004763A7},
 	}
+	
 	local gameNpcRefs = {
-		[1] = {0x00445B41, 0x00445C9D, 0x416AEF, 0x00420C20, 0x00445A69, 0x00445AD5, 0x00445BA2, 0x00445C15, 0x00445D0E, 0x0044A5A0, 0x0045F1C3, 0x0045F91E, 0x0044BF16, 0x0044613D},
-		[2] = {0x420C90, 0x00446DC1, 0x004326A8, 0x00446D68, 0x0044A2E1, 0x0044AD77, 0x0044B73C, 0x0044BFA8, 0x0044713B, 0x00446D6F},
+		[1] = {0x00445B41, 0x00445C9D, 0x416AEF, 0x00420C20, 0x00445A69, 0x00445AD5, 0x00445BA2, 0x00445C15, 0x00445D0E, 0x0044A5A0, 0x0045F1C3, 0x0045F91E, 0x0044BF16, 0x0044613D, 0x00463528, 0x00491B23},
+		[2] = {0x420C90, 0x00446DC1, 0x004326A8, 0x00446D68, 0x0044A2E1, 0x0044AD77, 0x0044B73C, 0x0044BFA8, 0x0044713B, 0x00446D6F, 0x004763B3,
+			0x00491B3E, -- directly references Margaret the Docent's hired bit
+			0x0047638B, -- directly references Baby Dragon's hired bit
+			},
 		[3] = {0042E269},
 		[4] = {0x00430687},
 	}
+
 	local npcDataRefs = {
-		[1] = {0x0045F1D0},
+		[1] = {0x0045F1D0, 0x004613B2, 0x004646DD, 0x00465EBE, 0x00491B1E},
+		size = { -- npc data size and game.npc size is same (at least should be)
+			[1] = {0x00491B19}
+		}
 	}
+
+	-- REMEMBER TO make code copying new npcs from npcdata to savegame npcs if new added
+
 	local npcProfRefs = {
 		[3] = {0x737AB7, 0x416B8C, 0x420CA8, 0x00445523, 0x44536B, 0x00445545, 0x004455AD, 0x0044551A, 0x004455A4},
 	}
+
 	local npcGroupRefs = {
-		[1] = {0x0045F229, 0x0045F98A},
-		[4] = {0x4224F5, 0x00446FD1},
+		[1] = {0x0045F229, 0x0045F98A, 0x00491B34},
+		[4] = {0x4224F5, 0x00446FD1, 0x0046A572},
+		size = {
+			[1] = {0x00491B2D,},
+		}
 	}
+	-- TODO: 0x491B2F has npc groups, which are copied into Game.NPCGroup (or vice versa) at 0x491B2D. Investigate this
+
 	local npcNewsRefs = {
-		[3] = {0x422509},
+		[3] = {0x422509, 0x0046A586},
 	}
+
 	local streetNpcRefs = {
-		[1] = {0x00445A84, 0x00445BA2},
+		[1] = {0x00445A84, 0x00445BA2, 0x004613AB},
+		count = {
+			[1] = {0x0046139E, 0x004613BC},
+			[2] = {0x0046117C, 0x004613C6},
+		},
 	}
-	local streetNpcSizeRefs = {
-		[1] = {0x0046139E,},
-		[2] = {0x0046117C,},
+
+	local npcNamesRefs = {
+		-- TODO: forgot about this table
+		count = {
+			[3] = {0x0048E9CA}
+		}
 	}
+
 	autohook(0x476CD5, function(d)
 		-- just loaded npcdata.txt, eax = data pointer, esi = space for processed data
 		local count = DataTables.ComputeRowCountInPChar(d.eax, 16, 16)
