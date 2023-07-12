7882ff1a2a181bcc6f6564f41fff9d34873d095b
Eksekk <eksekk3@gmail.com>
2023-07-12 22:17:12 +0200

    More refs

--- MM7/Structs/After/removeNpcLimits.lua
+++ MM7/Structs/After/removeNpcLimits.lua
@@ -16,7 +16,7 @@ end
 do
 	--[[
 		local format = string.format
-		local arrs = {"NPCDataTxt", "NPC", "NPCProfTxt", "NPCProfNames", "NPCTopic", "NPCText", "NPCNews", "NPCGroup", "NPCGreet", "StreetNPC"}
+		local arrs = {"NPCDataTxt", "NPC", "NPCProfTxt", "NPCProfNames", "NPCTopic", "NPCText", "NPCNews", "NPCGroup", "NPCGreet", "StreetNPC", "NPCNames"}
 		local out = {}
 		for k, name in pairs(arrs) do
 			local s = Game[name]
@@ -30,10 +30,9 @@ do
 	]]
 
 	--[[
-		NPCTopic:       low = 0x7214E8    high = 0x722D90   size = 0x18A8     itemSize = 0x8
-		NPCText:        low = 0x7214EC    high = 0x722D94   size = 0x18A8     itemSize = 0x8
 		NPCDataTxt:     low = 0x724050    high = 0x72D50C   size = 0x94BC     itemSize = 0x4C
 		NPC:            low = 0x72D50C    high = 0x7369C8   size = 0x94BC     itemSize = 0x4C
+		NPCNames:       low = 0x7369C8    high = 0x737AA8   size = 0x10E0     itemSize = 0x8
 		NPCProfTxt:     low = 0x737AA8    high = 0x737F44   size = 0x49C      itemSize = 0x14
 		StreetNPC:      low = 0x737F44    high = 0x739CF4   size = 0x0        itemSize = 0x4C
 		NPCNews:        low = 0x739CF4    high = 0x739DC0   size = 0xCC       itemSize = 0x4
@@ -42,18 +41,15 @@ do
 		NPCProfNames:   low = 0x73C110    high = 0x73C1FC   size = 0xEC       itemSize = 0x4
 	]]
 
-	-- maybe unrelated address: 0x4764B4, references 0x724048
+	-- maybe unrelated addresses: 0x4764B4, 0x4BC81E, 0x4BC82C - reference 0x724048
 
 	-- FIXME: NPCProfNames wasn't included in initial search!
 	
 	-- npc tables data range: 0x724004 - 0x73C027
-	local npcLimitRefs = { -- [cmd offset] = {addresses...}
-		[2] = {0x416AE4, 0x416B3D, 0x420C18, 0x420C6F, 0x0042E25B, 0x0042E2D0, 0x0043067B, 0x004306F0, 0x00445AC4, 0x00445B1F, 0x00445C06, 0x00445C67, 0x00445CFE, 0x00445D53, 0x00446132, 0x00446187, 0x0044A597, 0x0044A5E3, 0x0044BF0E, 0x0044BF2E, 0x00463520, 0x00463539, 0x004763A7},
-	}
 	
-	local gameNpcRefs = {
-		[1] = {0x00445B41, 0x00445C9D, 0x416AEF, 0x00420C20, 0x00445A69, 0x00445AD5, 0x00445BA2, 0x00445C15, 0x00445D0E, 0x0044A5A0, 0x0045F1C3, 0x0045F91E, 0x0044BF16, 0x0044613D, 0x00463528, 0x00491B23},
-		[2] = {0x420C90, 0x00446DC1, 0x004326A8, 0x00446D68, 0x0044A2E1, 0x0044AD77, 0x0044B73C, 0x0044BFA8, 0x0044713B, 0x00446D6F, 0x004763B3,
+	local gameNpcRefs = { -- [cmd offset] = {addresses...}
+		[1] = {0x00445B41, 0x00445C9D, 0x416AEF, 0x00420C20, 0x00445A69, 0x00445AD5, 0x00445BA2, 0x00445C15, 0x00445D0E, 0x0044A5A0, 0x0045F1C3, 0x0045F91E, 0x0044BF16, 0x0044613D, 0x00463528, 0x00491B23, 0x00491FCA, 0x00494156, 0x004BC484, 0x004BC589},
+		[2] = {0x420C90, 0x00446DC1, 0x004326A8, 0x00446D68, 0x0044A2E1, 0x0044AD77, 0x0044B73C, 0x0044BFA8, 0x0044713B, 0x00446D6F, 0x004763B3, 0x0049214F,
 			0x00491B3E, -- directly references Margaret the Docent's hired bit
 			0x0047638B, -- directly references Baby Dragon's hired bit
 			},
@@ -65,13 +61,17 @@ do
 		[1] = {0x0045F1D0, 0x004613B2, 0x004646DD, 0x00465EBE, 0x00491B1E},
 		size = { -- npc data size and game.npc size is same (at least should be)
 			[1] = {0x00491B19}
+		},
+		limit = { -- same as above
+			[2] = {0x416AE4, 0x416B3D, 0x420C18, 0x420C6F, 0x0042E25B, 0x0042E2D0, 0x0043067B, 0x004306F0, 0x00445AC4, 0x00445B1F, 0x00445C06, 0x00445C67, 0x00445CFE, 0x00445D53, 0x00446132, 0x00446187, 0x0044A597, 0x0044A5E3, 0x0044BF0E, 0x0044BF2E, 0x00463520, 0x00463539, 0x004763A7, 0x00491FC2, 0x00492017, 0x00494142, 0x00494161, 0x004BC478, 0x004BC4A7, 0x004BC581, 0x004BC5A8},
 		}
 	}
 
 	-- REMEMBER TO make code copying new npcs from npcdata to savegame npcs if new added
 
+	-- TODO? Grayface removed npc prof limits? might be unnecessary
 	local npcProfRefs = {
-		[3] = {0x737AB7, 0x416B8C, 0x420CA8, 0x00445523, 0x44536B, 0x00445545, 0x004455AD, 0x0044551A, 0x004455A4},
+		[3] = {0x737AB7, 0x416B8C, 0x420CA8, 0x00445523, 0x44536B, 0x00445545, 0x004455AD, 0x0044551A, 0x004455A4, 0x0049597C, 0x004B1FE2, 0x004B228C, 0x004B2295, 0x004B22EA, 0x004B2364, 0x004B3DD9, 0x004B4101, 0x004BC67D},
 	}
 
 	local npcGroupRefs = {
@@ -97,9 +97,27 @@ do
 
 	local npcNamesRefs = {
 		-- TODO: forgot about this table
+		[2] = {0x004953BD},
+		[3] = {0x0049543D},
 		count = {
+			[2] = {0x004953B5, 0x004953FD, 0x00495427},
 			[3] = {0x0048E9CA}
 		}
+	}
+
+	--[[
+	NPCTopic:       low = 0x7214E8    high = 0x722D90   size = 0x18A8     itemSize = 0x8
+	NPCText:        low = 0x7214EC    high = 0x722D94   size = 0x18A8     itemSize = 0x8
+	]]
+
+	-- NPC TOPIC ref ends with 8 or 0, NPC TEXT with 4 or C
+
+	local npcTopicRefs = {
+
+	}
+
+	local npcTextRefs = {
+
 	}
 
 	autohook(0x476CD5, function(d)
